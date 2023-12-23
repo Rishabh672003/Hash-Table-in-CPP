08b0f5c2d1665e6cd6b0f808dccffa0058753643
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2023-12-23 16:43:55 +0530

    fix the memory leak and use nullptr instead also make constexpr
    - fixed the destructor so now the program doesnt leak memory(checked with valgrind)
    - in the constructor fill the vector with nullptr and use it instead of NULL everywhere
    - make the hash generators constexpr and make stuff const

--- src/hash_table.cpp
+++ src/hash_table.cpp
@@ -3,15 +3,22 @@
 
 #include "hash_table.hpp"
 
+#include <algorithm>
 #include <cmath>
 #include <vector>
 
 ht_hash_table::ht_hash_table(int size) : size(size) {
     hash_table.reserve(size);
+    std::fill(hash_table.begin(), hash_table.begin() + size, nullptr);
 }
 ht_hash_table::~ht_hash_table() {
-    for (std::vector<ht_item*>::size_type i = 0; i < hash_table.size(); i++) {
-        delete hash_table[i];
+    for (int i = 0; i < size; ++i) {
+        ht_item* current = hash_table[i];
+        while (current != nullptr) {
+            ht_item* prev = current;
+            current = hash_table[++i];
+            delete prev;
+        }
     }
 }
 
@@ -19,15 +26,17 @@ void ht_hash_table::insert(const std::string& key, const std::string& value) {
     ht_item* new_item = new ht_item(key, value);
     int index = ht_get_hash(new_item->key, this->size, 0);
     ht_item* curr_item = hash_table[index];
-    if (curr_item == NULL) {
+    if (curr_item == nullptr) {
         hash_table[index] = new_item;
+        new_item = nullptr;
         this->count++;
     } else {
         int i = 1;
-        while (curr_item != NULL) {
+        while (curr_item != nullptr) {
             if (curr_item->key == key) {
                 delete curr_item;
                 hash_table[index] = new_item;
+                new_item = nullptr;
                 return;
             }
             index = ht_get_hash(new_item->key, this->size, i);
@@ -43,7 +52,7 @@ std::string ht_hash_table::search(const std::string& key) {
     int index = ht_get_hash(key, this->size, 0);
     ht_item* item = hash_table[index];
     int i = 1;
-    while (item != NULL) {
+    while (item != nullptr) {
         if (item->key == key) {
             return item->value;
         }
@@ -59,9 +68,10 @@ void ht_hash_table::remove(const std::string& key) {
     int index = ht_get_hash(key, this->size, 0);
     ht_item* item = hash_table[index];
     int i = 1;
-    while (item != NULL) {
+    while (item != nullptr) {
         if (item->key == key) {
             delete hash_table[index];
+            hash_table[index] = nullptr;
         }
         index = ht_get_hash(key, this->size, i);
         item = hash_table[index];
@@ -72,7 +82,8 @@ void ht_hash_table::remove(const std::string& key) {
 
 std::vector<ht_item*> ht_hash_table::getHashTable() const { return hash_table; }
 
-int ht_hash_table::hash(const std::string& s, const int a, const int m) {
+constexpr int ht_hash_table::hash(const std::string& s, const int a,
+                              const int m) const {
     long hash = 0;
     const int len_s = s.size();
     for (int i = 0; i < len_s; i++) {
@@ -82,8 +93,9 @@ int ht_hash_table::hash(const std::string& s, const int a, const int m) {
     return (int)hash;
 }
 
-int ht_hash_table::ht_get_hash(const std::string& s, const int num_buckets,
-                               const int attempts) {
+constexpr int ht_hash_table::ht_get_hash(const std::string& s,
+                                     const int num_buckets,
+                                     const int attempts) const {
     const int hash_a = hash(s, HT_PRIME_1, num_buckets);
     const int hash_b = hash(s, HT_PRIME_2, num_buckets);
     return (hash_a + (attempts * (hash_b + 1))) % num_buckets;
