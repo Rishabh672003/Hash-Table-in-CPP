de7b86cd78e421cb9f997910b56ab98819bf505f
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-01-01 13:03:53 +0530

    simplify stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
 #include "hash_table.hpp"
 #include <fmt/core.h>
 
-using std::string, fmt::print;
-
 int main() {
     ht_hash_table hash_table;
     hash_table.insert("Hi", "Mushashi");
     hash_table.insert("Hi", "Mob");
     hash_table.remove("Hi");
     hash_table.insert("Hello", "World");
-    print("{}\n", hash_table.search("Hi"));
+    fmt::print("{}\n", hash_table.search("Hi"));
     return 0;
 }
