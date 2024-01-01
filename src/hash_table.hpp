aa6b3083db1b4bff4f4780b965ef41c04b124d4d
Rishabh <53911515+Rishabh672003@users.noreply.github.com>
2024-01-01 13:03:44 +0530

    follow the rule of 5/7

--- src/hash_table.hpp
+++ src/hash_table.hpp
@@ -21,6 +21,12 @@ class ht_hash_table {
 
   public:
     explicit ht_hash_table(int size = 53);
+    ht_hash_table(const ht_hash_table&) = default;
+    ht_hash_table(ht_hash_table&&) = delete;
+    ht_hash_table& operator=(const ht_hash_table&) = default;
+    ht_hash_table& operator=(ht_hash_table&&) = delete;
+    explicit ht_hash_table(std::vector<ht_item*> hash_table)
+        : hash_table(std::move(hash_table)) {}
     virtual ~ht_hash_table();
 
     // Hash table API
@@ -32,7 +38,7 @@ class ht_hash_table {
     std::vector<ht_item*> getHashTable() const;
     constexpr int hash(const std::string& s, const int a, const int b) const;
     constexpr int ht_get_hash(const std::string& s, const int num_buckets,
-                    const int attempts) const;
+                              const int attempts) const;
 };
 
 #endif
